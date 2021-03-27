0009c7d6fc791959278887b5995610e916bb4bcb
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2021-03-27 22:28:32 +0900

    abc192c

--- abc192/c/main.cpp
+++ abc192/c/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 const int INF = 1e9;
 
 int main(){
  
-  int a;
-	std::cin>> a ;
+   int n,k;
+	 cin >> n >> k;
 
-	std::cout << a;
+	 string s1 = to_string(n);
+	 istringstream ss;
+
+	 for(int i = 0; i < k; i++){
+		 int g1,g2;
+		 sort(s1.begin(), s1.end(), greater<char>());
+		 ss = istringstream(s1);
+		 ss >> g1;
+
+		 sort(s1.begin(), s1.end());
+		 ss = istringstream(s1);
+		 ss >> g2;
+
+		 s1 = to_string(g1 - g2);
+	 }
+	 
+	 cout << s1 << endl;
  
-	return 0;
+	 return 0;
 }
