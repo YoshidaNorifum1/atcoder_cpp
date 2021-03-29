200f677dbba9b99ec01c495566e260b9afb25121
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2021-03-29 21:22:04 +0900

    agc014_a

---
+++ agc014/a/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+
+
+using namespace std;
+
+const int INF = 1e9;
+
+int main(){
+ 
+  int a,b,c;
+	std::cin>> a >> b >> c;
+	int res = 0;
+
+	if(a == b && b == c && a % 2 == 0){
+		res = -1;
+	}else{
+		while((a % 2 == 0) && (b % 2 == 0) && (c % 2 == 0)){
+			int tmpa = (b + c) / 2;
+			int tmpb = (a + c) / 2;
+			int tmpc = (a + b) / 2;
+			a = tmpa;
+			b = tmpb;
+			c = tmpc;
+			res += 1;
+		}
+	}
+
+	std::cout << res << endl;
+ 
+	return 0;
+}
---
+++ agc014/d/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+
+
+using namespace std;
+
+const int INF = 1e9;
+
+int main(){
+ 
+  int a;
+	std::cin>> a ;
+
+	std::cout << a << endl;
+ 
+	return 0;
+}
